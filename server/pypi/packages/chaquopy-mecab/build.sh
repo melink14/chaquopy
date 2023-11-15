acfb6cf466fdda9f0630c64ff0caadc5e265fdbe
Erek Speed <melink14@gmail.com>
2023-11-15 08:43:41 +0000

    Fixes to make building work
    clean so that old artifacts don't cause problems
    Add fPIC in 18 different ways. Need to figure out which ones actually do anything.
    Probably can add back the rm lines.
    
    Need setup tools for fugashi otherwise it complains.
    
    Thinking about it maybe the fpic flags weren't enough and perhaps  a patch is also required.
    
    Maybe adding fpic to the Makefile was also required for some reason. Will need to look into it.
    It was definitely required none of the ways to set flags in build.sh worked for some reason.
    The Makefile is generated but I can't figure out how it works.
    
    setup.py in fugashi also needs extra linker args too I think:
    extra_link_args=['-lc++_shared'])

--- server/pypi/packages/chaquopy-mecab/build.sh
+++ server/pypi/packages/chaquopy-mecab/build.sh
@@ -4,11 +4,14 @@ set -eu
 HOST_TRIPLET=$(basename $CC | sed 's/-gcc$//')
 
 cd mecab
+
 ./autogen.sh
-./configure --host=$HOST_TRIPLET --enable-utf8-only
+./configure CFLAGS='-fPIC' CXXFLAGS='-fPIC' --host=$HOST_TRIPLET --enable-utf8-only
+CFLAGS='-fPIC'
+make clean
 make -j $CPU_COUNT
 make install prefix=$PREFIX
 
-rm -r $PREFIX/bin
-rm $PREFIX/lib/*.a
+# rm -r $PREFIX/bin
+# rm $PREFIX/lib/*.a
 rm -r $PREFIX/share/man
